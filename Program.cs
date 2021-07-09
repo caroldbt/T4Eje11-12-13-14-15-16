309a1f681d0bebf9e44297a1097fe9c247920481
caroldbt <carol123dbt@gmail.com>
2021-07-10 00:13:40 +0200

    Eje1116

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Eje1116
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
